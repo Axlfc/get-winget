fd104f230d625acc1725c392c559360f5a7932e4
axel <axelfernandezcurros@gmail.com>
2021-09-08 09:49:23 +0200

    Adding logic to script

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,1 +1,8 @@
+$MyLink = "https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Write-Host "Congratulations! Your first script executed successfully"
+
+<#
+  Ensure a concrete path to perform the download and execution of the file.
+#>
+
+# Invoke-WebRequest $MyLink -OutFile winget-installer.msixbundle
