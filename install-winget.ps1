4d4c6d8f87d92acf13e06a796ff8aaab0c830f0a
Axel <axelfernandezcurros@gmail.com>
2021-09-28 11:29:05 +0200

    Update install-winget.ps1

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,4 +1,6 @@
+Set-ExecutionPolicy RemoteSigned
 $MyLink = "https://github.com/microsoft/winget-cli/releases/download/v1.0.12576/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
 
 Write-Host "Winget is being downloaded"
 
@@ -6,7 +8,9 @@ Write-Host "Winget is being downloaded"
   Ensure a concrete path to perform the download and execution of the file.
 #>
 
-Invoke-WebRequest $MyLink -OutFile winget-installer.msixbundle
+Invoke-WebRequest -Uri $MyLink -OutFile winget-installer.msixbundle
+
+Add-AppxPackage -Path .\winget-installer.msixbundle -ForceUpdateFromAnyVersion -Confirm -ForceTargetApplicationShutdown
 
 Write-Host "Winget installer downloaded, launching installer."
 .\winget-installer.msixbundle
