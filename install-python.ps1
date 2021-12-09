2f88e79cfd2b61bf80683f2e68d3d9c417c7c6e5
Axlfc <axelfernandezcurros@gmail.com>
2021-12-09 21:20:13 +0100

    CREATED: Install python test

---
+++ install-python.ps1
@@ -0,0 +1,16 @@
+Set-ExecutionPolicy RemoteSigned
+$MyLink = "https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe"
+
+
+Write-Host "Python is being downloaded"
+
+<#
+  Ensure a concrete path to perform the download and execution of the file.
+#>
+
+Invoke-WebRequest -Uri $MyLink -OutFile python-installer.msixbundle
+
+Add-AppxPackage -Path .\python-installer.exe -ForceUpdateFromAnyVersion -Confirm -ForceTargetApplicationShutdown
+
+Write-Host "python installer downloaded, launching installer."
+.\winget-installer.exe
