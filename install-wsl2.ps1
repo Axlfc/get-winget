6b8bdafb12637a06068582990ed6158fea0c99df
Axel <axelfernandezcurros@gmail.com>
2022-04-07 14:25:06 +0200

    Update install-wsl2.ps1

--- install-wsl2.ps1
+++ install-wsl2.ps1
@@ -3,17 +3,17 @@ Set-ExecutionPolicy RemoteSigned
 Write-Host "DO: Installing WSL2"
 # https://thomasward.com/wsl2-x11/ <- Reference
 
-$MyLink = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+#$MyLink = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
-Invoke-WebRequest -Uri $MyLink -OutFile wsl2-linux-kernel-installer.msixbundle
+#Invoke-WebRequest -Uri $MyLink -OutFile wsl2-linux-kernel-installer.msixbundle
 Write-Host "WSL2 installer downloaded, launching installer."
 #Add-AppxPackage -Path .\wsl2-linux-kernel-installer.msixbundle -ForceUpdateFromAnyVersion -ForceTargetApplicationShutdown
-.\wsl2-linux-kernel-installer.msixbundle
+#.\wsl2-linux-kernel-installer.msixbundle
+wsl --install -d "Ubuntu"
 wsl --set-default-version 2
 #winget install -s msstore "Ubuntu"
 #winget install -s msstore "Debian"
-wsl --install -d "Ubuntu"
 
 
 Write-Host "DO: Configuring Windows firewall to only allow connections to the X11 display server from the WSL2 instance running on your computer."
