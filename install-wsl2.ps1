9f4d7280b59e920b658445caf09aee34b22ebd25
Axel <axelfernandezcurros@gmail.com>
2022-04-08 10:31:49 +0200

    Update install-wsl2.ps1

--- install-wsl2.ps1
+++ install-wsl2.ps1
@@ -21,10 +21,12 @@ if ( ! $condition )
 {
     Write-Host "WSL2 launching update file download"
     Invoke-WebRequest -Uri $MyLink -OutFile wsl2-linux-kernel-installer.msi
+    Write-Host "WSL2 launching update install"
+    msiexec /qn /i $(echo $AppPath)
 }
 
 #Add-AppxPackage -Path $(echo $AppPath) -ForceUpdateFromAnyVersion -ForceTargetApplicationShutdown
-msiexec /qn /i $(echo $AppPath)
+
 #wsl --unregister "Ubuntu"
 wsl --install -d "Ubuntu"
 
