56ebb037c327dd9cd0b1e29893e83020176d86d0
Axel <axelfernandezcurros@gmail.com>
2022-04-08 14:43:04 +0200

    Update add_wsl2_dependencies.ps1

--- add_wsl2_dependencies.ps1
+++ add_wsl2_dependencies.ps1
@@ -1,4 +1,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
-#Enable-WindowsOptionalFeature -Online -FeatureName HypervisorPlatform -All -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Enable-WindowsOptionalFeature -Online -FeatureName HypervisorPlatform -All -NoRestart
+
+#Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
