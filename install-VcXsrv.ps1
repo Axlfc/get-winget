1978de6617b10184f5c26b137352814de1f37180
Axel <axelfernandezcurros@gmail.com>
2023-03-06 23:02:03 +0100

    UPDATED:  Adding VcXsrv to autostart

--- install-VcXsrv.ps1
+++ install-VcXsrv.ps1
@@ -1,2 +1,7 @@
 winget install --accept-package-agreements --accept-source-agreements -h --id "marha.VcXsrv"
-# Move program shortcut to autostart.
+# Moving program shortcut to autostart.
+# Example Get-ChildItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse | Where-Object { $_.Name -like '*.lnk' -and $_.Name -like '*adobe*' } | ForEach-Object { $_.FullName }
+$programDirectLink="$(Get-ChildItem "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse -Directory | Where-Object { $_.Name -like '*VcXsrv*' } | Get-ChildItem -Filter '*.lnk' | Where-Object { $_.Name -notlike '*Uninstall*' } | ForEach-Object { $_.FullName })"
+$username="$($env:USERNAME)"
+$windowsAutostartPath="C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+cp $programDirectLink $windowsAutostartPath
