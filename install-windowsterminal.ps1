893640f4cbcb2b1eb18aa7b61e571c84f7474746
Axel <axelfernandezcurros@gmail.com>
2023-04-13 14:40:34 +0200

    Update install-windowsterminal.ps1

--- install-windowsterminal.ps1
+++ install-windowsterminal.ps1
@@ -1,3 +1,7 @@
 winget install --accept-package-agreements --accept-source-agreements -h --id "Microsoft.WindowsTerminal"
 
 ## Add links in path
+
+## TODO: Create custom desktop shortcut for the terminal or copy it from somewhere, 
+## save it in C:\Users\<UserName>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs and add Ctrl + Alt + T in the shortcut key section 
+
