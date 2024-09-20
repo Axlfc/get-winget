0381b7ca523cfffa6ee915253bf8241e2c66c8e2
Axel <axelfernandezcurros@gmail.com>
2024-09-20 11:19:09 +0200

    UPDATED: function Add-PathAndAlias
    New function to add Path and Alias function (pycharm example)

--- Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
@@ -449,3 +449,55 @@ function today_function {
 	return Get-Date
 }
 Set-Alias today today_function
+
+
+function Add-PathAndAlias {
+    param (
+        [string]$path,
+        [string]$alias,
+        [string]$executable
+    )
+
+    # Validate the provided path
+    if (-not (Test-Path $path)) {
+        Write-Host "The path '$path' does not exist." -ForegroundColor Red
+        return
+    }
+
+    # Check if the executable exists in the provided path
+    $executablePath = Join-Path -Path $path -ChildPath $executable
+    if (-not (Test-Path $executablePath)) {
+        Write-Host "The executable '$executable' does not exist in the path '$path'." -ForegroundColor Red
+        return
+    }
+
+    # Add the path to the PATH environment variable
+    if ($env:Path.Split(';') -notcontains $path) {
+        $env:Path += ";$path"
+        # Write-Host "Added '$path' to the PATH environment variable." -ForegroundColor Green
+    } else {
+        Write-Host "The path '$path' is already in the PATH environment variable." -ForegroundColor Yellow
+    }
+
+    # Create a new function that launches the executable with or without arguments
+    $functionDefinition = @"
+function global:$alias {
+    param(
+        [Parameter(ValueFromRemainingArguments=`$true)]
+        [string[]]`$args
+    )
+    
+    if (`$args.Count -eq 0) {
+        Start-Process -FilePath "$executablePath" -WindowStyle Hidden
+    } else {
+        Start-Process -FilePath "$executablePath" -ArgumentList `$args
+    }
+}
+"@
+    Invoke-Expression $functionDefinition
+    # Write-Host "Created function '$alias' to launch '$executable' with or without arguments." -ForegroundColor Green
+}
+
+# Example: 
+Add-PathAndAlias -alias "pycharm" -path "C:\Program Files (x86)\JetBrains\PyCharm Community Edition 2023.3.2\bin" -executable "pycharm64.exe"
+
